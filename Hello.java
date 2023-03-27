9c71f349441cc8dca45dac8e258340547adf2070
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2023-03-27 11:29:26 +0800

    init

---
+++ Hello.java
@@ -0,0 +1,9 @@
+// `Hello` is a public class
+public class Hello{
+// 中文注释。需要以GBK编码导出（因为windows的cmd的properties用的是GBK编码）
+    // `static` is sth in OOP 
+    public static void main(String[] args){
+        System.out.print("hello,你好");
+    }
+}
+
