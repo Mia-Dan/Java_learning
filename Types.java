a519b9d08994a9451bf47efb0e1316770dd7d76b
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2023-03-28 10:59:18 +0800

    types

---
+++ Types.java
@@ -0,0 +1,24 @@
+public class Types{
+
+    // class constant: available to multiple methods inside a single class
+    // `static` `final`
+    public static final double CM_PER_INCH1 = 2;
+
+    public static void main(String[] args){
+
+        int vacationDays;
+        vacationDays = 12;
+        System.out.println(vacationDays);
+
+        // constant: use `final` to denote constant
+        // `final`: its value is set once for all
+        final double CM_PER_INCH = 2.54;
+        double paperWidth = 8.5;
+        double paperHeight = 11;
+        System.out.println("Paper size in cms 0: "
+            + paperWidth * CM_PER_INCH + " by " + paperHeight * CM_PER_INCH);
+        System.out.println("Paper size in cms 1: "
+            + paperWidth * CM_PER_INCH1 + " by " + paperHeight * CM_PER_INCH1);
+
+    }
+}
