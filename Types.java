f1c6433374a9d9ae3c1bd8bfbc922ba69d421d50
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2023-03-30 15:16:24 +0800

    enumerate - TODO

--- Types.java
+++ Types.java
@@ -1,37 +1,34 @@
 public class Types{
 
     // class constant: available to multiple methods inside a single class
-    // `static` `final`
     public static final double CM_PER_INCH = 2;
+
+    // Enmuerate Type
+    enum Size{SMALL, MEDIUM, LARGE, EXTRA_LARGE}; 
+    // enum Num{0,1,2}; // TODO: why error
 
     public static void main(String[] args){
 
         int vacationDays;
-        vacationDays = 12;
-        System.out.println(vacationDays);
+        final double CM_PER_INCH = 2.54; // constant, `final`: its value is set once for all
 
-        // constant: use `final` to denote constant
-        // `final`: its value is set once for all
-        final double CM_PER_INCH = 2.54;
-        double paperWidth = 8.5;
-        double paperHeight = 11;
-        System.out.println("Paper size in cms 0: "
-            + paperWidth * CM_PER_INCH + " by " + paperHeight * CM_PER_INCH);
+/* Compiler:
+Types.java: error: enum types must not be local
+        enum Size{SMALL, MEDIUM, LARGE, EXTRA_LARGE};
+        ^
+1 error
+*/
+        // enum Size{SMALL, MEDIUM, LARGE, EXTRA_LARGE}; //error
 
-        // the res uses CM_PER_INCH = 2.54
-        // overwrite
+        Size s;
+        // System.out.println(s); // error: s might not have been initialized
 
-        double cm_per_inch1 = func1();
-        System.out.println("Paper size in cms func1: "
-            + paperWidth * cm_per_inch1 + " by " + paperHeight * cm_per_inch1);
+        s = Size.MEDIUM;
+        System.out.println(s); //MEDIUM
 
-        // the res uses CM_PER_INCH = 2
-        // func1() uses class constant
+        // Num n = Num.0;
+        // System.out.println(Num+1);
+
     }
 
-    public static double func1(){
-        // to figure out which constant CM_PER_INCH would be use
-
-        return CM_PER_INCH;
-    }
 }
