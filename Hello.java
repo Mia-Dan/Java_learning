db16c0da9b6179d2ce043edd5a2b71fc9e4f2e22
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2023-03-30 15:52:12 +0800

    basics

--- Hello.java
+++ Hello.java
@@ -1,12 +1,32 @@
 // Comments
-// 中文注释。需要以GBK编码导出（因为windows的cmd的properties用的是GBK编码）; For Linux, it's safe
+
+// 1. one public class per file.
+// 2. file name == public class name
+//  e.g., this file is, Hello.java
 
 // `Hello` is a public class
+// compile by `javac Hello.java`
+// run by `java Hello`
 public class Hello{
 
-    // `static` is sth in OOP 
     public static void main(String[] args){
-        System.out.println("hello, 你好"); // System.out object; println() method
+
+        System.out.println("This is public class `Hello`");
+    }
+
+}
+
+// compile by `javac Hello.java`
+// run by `java Cat`
+class Cat{
+    public static void main(String[] args){
+        System.out.println("Meow~");
     }
 }
 
+// private class Rat{// error: modifier private not allowed here
+class Rat{
+    public static void main(String[] args){
+        System.out.println("...");
+    }
+}
