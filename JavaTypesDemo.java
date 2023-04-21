df0c8fdf7df686688396936057488da950e7c2e4
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2023-04-21 16:20:43 +0800

    3 - Java Types Categories

---
+++ JavaTypesDemo.java
@@ -0,0 +1,41 @@
+public class JavaTypesDemo {
+
+  public static void main(String[] args) {
+  /* there are two categories of types in Java: 
+  - primitive types
+  - reference types
+  The primitive types are the boolean type and the numeric types which include byte, short, int, long, char, float and double. 
+    The term "primitive" in Java refers to a data type that is not an object and has no methods. 
+    Primitive types are the most basic data types available within the Java language. 
+    They serve as the building blocks of data manipulation in Java and contain pure, simple values of a kind.
+  The reference types include class types, interface types and array types.
+    Reference types are types that are defined by a class or an interface. 
+    They are called reference types because they refer to objects.
+  */
+
+    // 1. prmitive types
+
+    // numerical
+    byte b = 100;
+    short s = 20000;
+    int i = 1000000;
+    long l = 1000000000000L;
+    float f = 3.14f;
+    double d = 3.141592653589793;
+
+    // char
+    char c = 'A';
+
+    // boolean
+    boolean bool = true;
+
+    // 2. reference types
+    // class
+    // String is one kind of class
+
+    // interface
+
+    // array
+  }
+
+}
