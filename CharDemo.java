dccba2d3e5215e8d380dee56807704ff6f80c87d
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2023-04-23 20:34:58 +0800

    3 - Types Char

--- CharDemo.java
+++ CharDemo.java
@@ -1,12 +1,18 @@
+/** 
+ * details about type char,
+ * and character encoding.
+ */
 public class CharDemo {
 
     public static void main(String[] args) {
+
         // 1 declaration & assignment
         //  char constant is quoted by ''
         //  while "" indicates String
-        char ch = 'a'; 
+
+        char ch = '中';
         System.out.println(ch);
-        ch = '中';
+        ch = 'a'; 
         System.out.println(ch);
         ch = 97; // 'a'
         System.out.println(ch);
@@ -14,6 +20,40 @@ public class CharDemo {
         System.out.println(ch);
 
         // 2 representation & storage
+        //  char is essentially integer
+        //  when output, it looks up `unicode` table
+
+        char ch1 = 'a';
+        System.out.println(ch1);
+        System.out.println((int)ch1); // explicit type convertion
+        char ch2 = '中'; 
+        System.out.println(ch2);
+        System.out.println((int)ch2); // 20013
+        char ch3 = 20013; // '中'
+        System.out.println(ch3);
+        System.out.println((int)ch3); 
+
+        //  ->
+        //  can do operations like `+` on char
+        //   between char and int
+        char ch4 = 'a' + 1;
+        System.out.println(ch4);
+        System.out.println((int)ch4); 
+
+        //   between char and char
+        ch4 = 'b' 
+            + ('A' - 'a'); // which is diff from lower to upper case
+        System.out.println(ch4);
+
+        ch4 = 'k';
+        if (ch4 >= 'a' && ch4 <= 'z') { // if lower case
+            // ch4 = ch4 + ('A' - 'a'); // ERROR: ...
+            // incompatible types: possible lossy conversion from int to char
+            ch4 = (char) (ch4 + ('A' - 'a')); // turn upper case
+        }
+        System.out.println(ch4); 
+        System.out.println((int)ch4); 
+
     }
     
 }
