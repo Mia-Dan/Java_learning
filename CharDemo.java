ce79a27464b136bd98a75a3d9b3e7ce174c49c4c
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2023-04-23 20:43:05 +0800

    3 - Types Char

--- CharDemo.java
+++ CharDemo.java
@@ -19,6 +19,7 @@ public class CharDemo {
         ch = '\n';
         System.out.println(ch);
 
+
         // 2 representation & storage
         //  char is essentially integer
         //  when output, it looks up `unicode` table
@@ -30,7 +31,7 @@ public class CharDemo {
         System.out.println(ch2);
         System.out.println((int)ch2); // 20013
         char ch3 = 20013; // '中'
-        System.out.println(ch3);
+        System.out.println(ch3); // 中
         System.out.println((int)ch3); 
 
         //  ->
@@ -43,17 +44,32 @@ public class CharDemo {
         //   between char and char
         ch4 = 'b' 
             + ('A' - 'a'); // which is diff from lower to upper case
-        System.out.println(ch4);
+        System.out.println(ch4); // B
 
-        ch4 = 'k';
+        //   uses
+        ch4 = 'k'; 
         if (ch4 >= 'a' && ch4 <= 'z') { // if lower case
             // ch4 = ch4 + ('A' - 'a'); // ERROR: ...
             // incompatible types: possible lossy conversion from int to char
             ch4 = (char) (ch4 + ('A' - 'a')); // turn upper case
         }
-        System.out.println(ch4); 
-        System.out.println((int)ch4); 
+        System.out.println(ch4); // K
+        System.out.println((int)ch4); // 75
 
+        //   uses
+        //   about ERROR: possible lossy conversion from int to char
+        //      1
+        ch4 = (char) ('A' - 'a');
+        // ch4 = 'A' - 'a'; // ERROR: possible lossy conversion from int to char
+        // GUESS: underflow causes ambiguity
+        System.out.println(ch4); // ￠
+        System.out.println((int)ch4); // 65504
+        //      2
+        ch4 = (char) ('a' - 'A');
+        ch4 = 'a' - 'A'; // works!
+        System.out.println(ch4); // (whitespace)
+        System.out.println((int)ch4); // 32
+ 
     }
     
 }
