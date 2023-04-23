6405bafc03dbc20602c6297d21fd4167a07fb917
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2023-04-23 20:11:31 +0800

    3 - char & encoding

---
+++ CharDemo.java
@@ -0,0 +1,19 @@
+public class CharDemo {
+
+    public static void main(String[] args) {
+        // 1 declaration & assignment
+        //  char constant is quoted by ''
+        //  while "" indicates String
+        char ch = 'a'; 
+        System.out.println(ch);
+        ch = '中';
+        System.out.println(ch);
+        ch = 97; // 'a'
+        System.out.println(ch);
+        ch = '\n';
+        System.out.println(ch);
+
+        // 2 representation & storage
+    }
+    
+}
