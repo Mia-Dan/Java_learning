0556f347a400cd3f8e2f43b215c2c810e2bfadfd
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2023-04-25 13:39:44 +0800

    3 - String Convertion

--- StringConvertionDemo.java
+++ StringConvertionDemo.java
@@ -27,7 +27,8 @@ public class StringConvertionDemo {
 
         String s6 = "hello world";
         int num5 = Integer.parseInt(s6);
-        /*
+        // compile succeed
+        /* runtime exception
         Exception in thread "main" java.lang.NumberFormatException: For input string: "hello world"
             at java.lang.NumberFormatException.forInputString(NumberFormatException.java:65)
             at java.lang.Integer.parseInt(Integer.java:580)
@@ -36,3 +37,46 @@ public class StringConvertionDemo {
         */
     }
 }
+
+/* Python version
+
+TLDR:
+other types -> String |  str()
+String -> other types |  int(), float(), ... | eval() 
+
+---
+
+#  prmitive types -> String
+n1 = 100
+f1 = 1.1
+d1 = 4.5
+b1 = True
+s1 = str(n1)
+s2 = ...
+print(s1 + " " + s2 + " " + s3 + " " + s4)
+
+---
+
+# String -> prmitive types
+
+s5 = "123"
+num1 = int(s5)
+num2 = float(s5)
+num3 = float(s5)
+num4 = int(s5)
+print(num1, num2, num3, num4)
+
+>>> eval("1") # <class 'int'>
+1
+>>> eval("1.2") # <class 'float'>
+1.2
+>>> eval("hello")
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "<string>", line 1, in <module>
+NameError: name 'hello' is not defined
+
+>>> eval("1+2+3") # <class 'int'>
+6
+
+*/
