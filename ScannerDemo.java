2e4cdd3bafacd79d7710fda5750510af8e9a410f
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2023-05-05 16:55:47 +0800

    4 - Scanner

---
+++ ScannerDemo.java
@@ -0,0 +1,22 @@
+// package: java.util
+// class: Scanner
+// look inside src/java/util/Scanner.java
+import java.util.Scanner;
+
+public class ScannerDemo{
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter username: ");
+        String usr = scanner.next(); //当程序执行到 next 方法时，会等待用户输入
+        System.out.println("Enter password: ");
+        String pwd = scanner.next();
+
+        System.out.println("\nWhat has just been entered: \n" + usr + "\n" + pwd); 
+
+        System.out.println("How do you rate this system?");
+        int rating = scanner.nextInt();   // convert to int, or raise exceptions
+    }
+}
