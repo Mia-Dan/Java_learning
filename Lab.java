0b15ee84a6aa6861de78d47e2e72b5454686d1db
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2023-05-05 20:02:41 +0800

    4 - lab

---
+++ Lab.java
@@ -0,0 +1,10 @@
+public class Lab{
+    
+    public static void main(String[] args) {
+        String s1 = "2";
+        double d = Double.parseDouble(s1);
+
+        char c = 'a';
+        String s2 = c + "";
+    }
+}
