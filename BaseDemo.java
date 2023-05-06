5584241189484d64b2fac1b93bae6f813a02da16
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2023-05-06 10:24:33 +0800

    4 - Base 进制

--- BaseDemo.java
+++ BaseDemo.java
@@ -1,22 +1,26 @@
+/** base 进制
+ * demonstrate the representation of binary, octal, decimal, hexadecimal literal in Java
+ */
+
 public class BaseDemo{
 
     public static void main(String[] args) {
         // 1010 interpreted as ...
 
-        // 2 binary number, 0b... or OB...
+        // 2 binary literal, 0b... or OB...
         int n1 = 0b1010;
-        // 8 octal number, 0...
+        // 8 octal literal, 0...
         int n3 = 01010;
-        // 10 decimal number
+        // 10 decimal literal
         int n2 = 1010;
-        // 16 hexadecimal number, 0x... or 0X...
+        // 16 hexadecimal literal, 0x... or 0X...
         int n4 = 0X10101;
 
         System.out.println("n1=" + n1);
         System.out.println("n2=" + n2);
         System.out.println("n3=" + n3);
         System.out.println("n4=" + n4);
-        // hexadecimal number
+        // hexadecimal literal, UPPERCASE and lowercase are both ok
         System.out.println(0x23A);
         System.out.println(0x23a);
     }
