2d9cd07d46755fa49c42f6cea6d94e9e0a2353ba
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2023-05-09 11:47:42 +0800

    5 - control statement - break, continue, return

--- ControlStatementDemo.java
+++ ControlStatementDemo.java
@@ -1,19 +1,112 @@
-// import java.util.Scanner;
+/**
+ * Demonstate of following control statement: 
+ * break, continue, return
+ * - break with / without labels
+ * - comparison between: break, continue, return
+ */
 
 public class ControlStatementDemo{
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 10; i++) {
-            if (i == 2) {
-                System.out.println("Surprise!");
-                System.out.println("break");
-                break;
+        // 1 Break with / without Labels
+
+        System.out.println("break without label ⬇️");
+        label1:
+        for (int i = 0; i < 5; i++) {
+            label2:
+            for (int j = 0; j < 5; j++){
+                label3:
+                System.out.println("i = " + i + ", j = " + j);
+                if (j == 1) {
+                    break;
+                }
             }
-            System.out.println("i = " + i);
+
         }
-        // Scanner sc = new Scanner(System.in);
-        System.out.println("Ending ...");
-        // sc.next();
+
+        System.out.println();
+
+        System.out.println("break label1 ⬇️");
+        label1:
+        for (int i = 0; i < 5; i++) {
+            label2:
+            for (int j = 0; j < 5; j++){
+                label3:
+                System.out.println("i = " + i + ", j = " + j);
+                if (j == 1) {
+                    break label1;
+                }
+            }
+
+        }
+
+        System.out.println();
+
+        System.out.println("break label2 ⬇️");
+        label1:
+        for (int i = 0; i < 5; i++) {
+            label2:
+            for (int j = 0; j < 5; j++){
+                label3:
+                System.out.println("i = " + i + ", j = " + j);
+                if (j == 1) {
+                    break label2;
+                }
+            }
+
+        }
+
+        // System.out.println("break label3 ⬇️");
+        // label1:
+        // for (int i = 0; i < 5; i++) {
+        //     label2:
+        //     for (int j = 0; j < 5; j++){
+        //         label3:
+        //         System.out.println("i = " + i + ", j = " + j);
+        //         if (j == 1) {
+        //             break label3; // ERROR: undefined label: label3
+        //         }
+        //     }
+        // }
+
+    System.out.println();
+    
+    // 2 Comparison Between: Break, Continue, Return
+
+    for (int i = 0; i < 10; i++) {
+        if (i == 2) {
+            System.out.println("Surprise!");
+            System.out.println("break");
+            break;
+        }
+        System.out.println("i = " + i);
     }
+    System.out.println("Ending ...");
+
+    System.out.println();
+
+    for (int i = 0; i < 10; i++) {
+        if (i == 2) {
+            System.out.println("Surprise!");
+            System.out.println("continue");
+            continue;
+        }
+        System.out.println("i = " + i);
+    }
+    System.out.println("Ending ...");
+
+    System.out.println();
+
+    for (int i = 0; i < 10; i++) {
+        if (i == 2) {
+            System.out.println("Surprise!");
+            System.out.println("return");
+            return;
+        }
+        System.out.println("i = " + i);
+    }
+    System.out.println("Ending ...");
+
+}
 }
