9983d04789367bca7f5493581c2329b39db822e8
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2023-05-24 16:46:24 +0800

    25 - jdbc with MySQL - check connection - refactor

--- code/jdbc_MySQL/connector/src/Check.java
+++ code/jdbc_MySQL/connector/src/Check.java
@@ -1,46 +1,56 @@
+/**
+ * Check MySQL Database Connection
+ *      by try accessing data in it
+ */
+
 import java.sql.*;
-import java.util.Scanner;
-// import com.mysql.jdbc.Driver; // Loading class `com.mysql.jdbc.Driver'. This is deprecated. The new driver class is `com.mysql.cj.jdbc.Driver'. The driver is automatically registered via the SPI and manual loading of the driver class is generally unnecessary.
 import com.mysql.cj.jdbc.Driver;
 
 public class Check {
 
     public static void main(String arg[])
     {
-        // Connection connection = null;
-        Scanner sc = new Scanner(System.in);
         try {
-//            sc.next();
 
-            // below two lines are used for connectivity.
-            // Class.forName("com.mysql.jdbc.Driver");
+            // 1. 向项目中导入jar包
+            // see notes
+
+            // 2. 得到连接 for connectivity.
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Driver d = new Driver();
-            Connection connection = null;
-            connection = DriverManager.getConnection(
+            Connection connection = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/testdb",
-                "root", "root1234");
-        // //     // mydb is database
-        // //     // root is username of database
-        // //     // root1234 is password of database
+                "root", "root1234"
+                );
+            // `jdbc:mysql://`: 协议，通过 jdbc 的方式连接 mysql
+            // `localhost`: 主机，可以是 ip 地址
+            // `3306`: MySQL 监听的端口
+            // mysql 的连接本质是 socket 连接
+            // `testdb`: name of database (in MySQL DBMS)
+            // `root`: username of database
+            // `root1234`: password of database
+
             System.out.println("Login success ");
             
-              Statement statement;
-              statement = connection.createStatement();
-              ResultSet resultSet;
-              resultSet = statement.executeQuery(
-                  "select * from designation");
-              int code;
-              String title;
-              while (resultSet.next()) {
-                  code = resultSet.getInt("code");
-                  title = resultSet.getString("title").trim();
-                  System.out.println("Code : " + code
-                                  + " Title : " + title);
-              }
-              resultSet.close();
-              statement.close();
-              connection.close();
+            //3. 执行 sql
+            // statement 用于执行静态 SQL 语句并返回其生成的结果的对象
+            Statement statement = connection.createStatement();
+            String sql = "select * from designation";
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            // 4. 使用结果（e.g., 输出）
+            int code;
+            String title;
+            while (resultSet.next()) {
+                code = resultSet.getInt("code");
+                title = resultSet.getString("title").trim();
+                System.out.println("Code : " + code + " Title : " + title);
+            }
+
+            // 5. 关闭连接资源
+            resultSet.close();
+            statement.close();
+            connection.close();
+
         }
         catch (Exception exception) {
             System.out.println(exception);
