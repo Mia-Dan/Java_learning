a0d2feeb34a8fd75ba501ebb2e6317d0295c9bbf
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2023-08-13 20:25:05 +0800

    21 - socketTCP 2 - how to show something at browser? - tmp

--- code/socket/TCP2/Server.java
+++ code/socket/TCP2/Server.java
@@ -4,18 +4,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import java.nio.charset.StandardCharsets;
 
 public class Server{
+
     public static void main(String[] args) throws IOException{
 
         ServerSocket serverSocket = new ServerSocket(7004);
         System.out.println("server listening at port 7004...");
 
         Socket socket = serverSocket.accept();
-        System.out.println("Server | socket =" + socket.getClass());
 
         // a. Receive from Client
         // bind socket's inputstream to inStream
+        System.out.println("Server | socket =" + socket.getClass());
+
         InputStream inStream = socket.getInputStream();
         // InputStream usage TODO
         byte[] buffer = new byte[1024];
@@ -29,7 +32,27 @@ public class Server{
         // bind socket's outputstream to inStream
         OutputStream outStream = socket.getOutputStream();
         // OutputStream usage
-        outStream.write("Hi, Mia (from server Lain)".getBytes()); 
+
+
+
+// TODO
+                Response response = new Response(outStream);
+                response.setRequest(request);
+                response.sendStaticResource();
+
+
+// loading ...?
+
+            String data = "<html><body><h1>Hello, world!</h1></body></html>";
+            int length = data.getBytes(StandardCharsets.UTF_8).length;
+            outStream.write("HTTP/1.1 200 OK\r\n".getBytes());
+            outStream.write("Connection: close\r\n".getBytes());
+            outStream.write("Content-Type: text/html\r\n".getBytes());
+            outStream.write(("Content-Length: " + length + "\r\n").getBytes());
+            outStream.write("\r\n".getBytes()); // 空行标识Header和Body的分隔
+            outStream.write(data.getBytes());
+
+
 
         socket.shutdownOutput();
 
